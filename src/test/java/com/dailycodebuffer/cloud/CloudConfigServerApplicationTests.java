2652e04d785437fad70e145a89e700a6222d6412
NaveedAmanat <naveed0004@gmail.com>
2022-12-03 15:12:12 +0500

    first commit

---
+++ src/test/java/com/dailycodebuffer/cloud/CloudConfigServerApplicationTests.java
@@ -0,0 +1,9 @@
+package com.dailycodebuffer.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+}
